f78a607744fcd55f2273c51f48d124a7ba04013b
Yours <2587433598@qq.com>
2022-11-28 22:44:50 +0800

    message

---
+++ src/main/java/com/example/cs307front/Cs307frontApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs307front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs307frontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs307frontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs307front/Cs307frontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs307front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs307frontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
